8d1c208fe48497c676eade1b020c09bf1622afd7
Thiago <thiago.tcsilva@outlook.com>
2022-10-12 17:13:51 -0300

    first-commit

---
+++ Python/fizz-buss.py
@@ -0,0 +1,15 @@
+
+def fb(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return f'fizzbuzz, {n} é divisível por 3 e 5'
+    if n % 5 == 0:
+        return f'buzz, {n} é divisível por 5'
+    if n % 3 == 0:
+        return f'fizz, {n} é divisível por 3'
+    return n
+
+from random import randint
+
+for i in range(100):
+    aleatorio = randint(0,100)
+    print(fb(aleatorio))
